0863ca6591f7be711a69f572058852b10d9a47b4
Jeremy Bouchard <JBouchard1134@gmail.com>
2020-08-22 14:04:15 -0700

    added moment.min.js integration

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-const busHours = [{hour: '9AM', task: ''}, {hour: '10AM', task: ''}, {hour: '11AM', task: ''}, {hour: '12PM', task: ''}, {hour: '1PM', task: ''}, {hour:'2PM', task: ''}, {hour: '3PM', task: ''}, {hour: '4PM', task: ''}, {hour: '5PM', task: ''}];
+const busHours = [{hour: '9AM', task: '', id: '09'}, {hour: '10AM', task: '', id: '10'}, {hour: '11AM', task: '', id: '11'}, {hour: '12PM', task: '', id: '12'}, {hour: '1PM', task: '', id: '13'}, {hour:'2PM', task: '', id: '14'}, {hour: '3PM', task: '', id: '15'}, {hour: '4PM', task: '', id: '16'}, {hour: '5PM', id: '17'}];
 
 // init();
 
@@ -11,27 +11,43 @@ for (let i = 0; i < busHours.length; i++) {
 
     // Shows time of day per row
     const $hour = $('<div>');
-    $hour.text(busHours[i]);
+    $hour.text(busHours[i].hour);
     $hour.addClass('col-sml-2');
     $($form).append($hour);
 
     // Adds text area
     const $myTask = $('<textarea>');
     $myTask.addClass('col-lg-9 text-area');
-    $myTask.attr({'id': busHours[i], 'data-letter': busHours[i]});
+    $myTask.attr({'id': busHours[i].hour});
     $($form).append($myTask);
 
     // Adds Save Button
     const $saveBtn = $('<button>');
     $saveBtn.addClass('col-md-1 save-btn');
     $saveBtn.text('Save');
-    $saveBtn.attr({'data-letter': busHours[i]});
+    $saveBtn.attr({'id': busHours[i].hour});
     $($form).append($saveBtn);
+
+    if (busHours[i].id < moment().format("HH")) {
+        $myTask.attr ({"class": "past"})
+    } else if (busHours[i].id === moment().format("HH")) {
+        $myTask.attr({"class": "present"});
+    } else if (busHours[i].id > moment().format("HH")) {
+        $myTask.attr({"class": "future"});
+        console.log(busHours[i].id)
+    }
 };
 
+// gets data for the header date
+function getDate() {
+    var currentDate = moment().format('dddd, MMMM Do');
+    $("#currentDay").text(currentDate);
+}
+
+// Render tasks into the textarea
 function renderTasks() {
     for (let i = 0; i < busHours.length; i++) {
-        $('.text-area').text(busHours.task[i]);
+        $('.text-area').text(myTasks[i]);
     }
 }
 
@@ -39,16 +55,18 @@ function init() {
     // Check for existing tasks
     if (localStorage.getItem('myTasks')) {
         const savedTasks = JSON.parse(localStorage.getItem('myTasks'))
-        busHours.task.push(...savedTasks);
-        console.log(busHours.task);
+        myTasks.push(...savedTasks);
+        console.log(myTasks);
     }
     renderTasks();
   }
 
-function storeTasks(time) {
-    // Store busHours.task to Local Storage
-    localStorage.setItem('myTasks', JSON.stringify(busHours.task));
+function storeTasks() {
+    // Store myTasks to Local Storage
+    localStorage.setItem('myTasks', JSON.stringify(myTasks));
   }
+
+getDate();
 
 $('.save-btn').on('click', function(event) {
     event.preventDefault();
@@ -56,7 +74,7 @@ $('.save-btn').on('click', function(event) {
     console.log(textFieldid);
     const myTask = $('#' + textFieldid).val().trim();
     console.log(myTask);
-    busHours.task.push(myTask);
-    storeTasks(textFieldid);
+    myTasks.push(myTask);
+    storeTasks();
 });  
 
